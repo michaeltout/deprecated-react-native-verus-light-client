4e5f2e001261eec26009beb3f7c6a0200a85fdcb
Diesmaster <chris@vanmaarseveen.net>
2020-04-18 14:30:30 +0200

    adroid: update kotlin

--- greeting/src/main/java/cash/z/wallet/sdk/KtJavaComLayer.kt
+++ greeting/src/main/java/cash/z/wallet/sdk/KtJavaComLayer.kt
@@ -450,7 +450,8 @@ class KtJavaComLayer (){
 			}
 			val aB = availableBalance.toString();
 			val tB = totalBalance.toString();
-			"total: " + tB + ", confirmed: " + aB + "";
+			val total =  tB + ", " + aB + "";
+			total;
 			}else{
 				"error: syncronizer has not been started";
 			}
