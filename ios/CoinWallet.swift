d47797d95ad178f1c18433d0dbeb550a8f72e8db
michaeltout <toutonghi.michael@gmail.com>
2021-01-28 17:08:35 +0100

    Share compact block cachedb between different accounts that use the same blockchain, derive viewing keys from passed in spending key

--- ios/CoinWallet.swift
+++ ios/CoinWallet.swift
@@ -79,7 +79,8 @@ class CoinWallet {
             self.synchronizer = try SDKSynchronizer(initializer: self.wallet!)
                    
             self.service = LightWalletGRPCService(endpoint: self.endpoint)
-            let _ = try self.wallet?.initialize(viewingKeys: try DerivationTool.default.deriveViewingKeys(seed: Array(self.seed.utf8), numberOfAccounts: self.accounts), walletBirthday: self.birthday, network: self.coinId)
+            
+            let _ = try self.wallet?.initialize(viewingKeys: [try DerivationTool.default.deriveViewingKey(spendingKey: self.seed)], walletBirthday: self.birthday, network: self.coinId)
             
             NotificationCenter.default.addObserver(self, selector: #selector(processorNotification(_:)), name: nil, object: wallet!.blockProcessor())
         }
